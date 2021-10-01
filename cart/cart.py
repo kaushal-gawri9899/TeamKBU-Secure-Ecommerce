4a53718c0477a2290ba809622ced2f6c6f9c2442
alitariqs3779826 <s3779826@student.rmit.edu.au>
2021-10-02 00:59:16 +1000

    add

--- cart/cart.py
+++ cart/cart.py
@@ -177,26 +177,22 @@ def editCart():
             return redirect(url_for('user_bp.login'))
 
         oid = request.values.get('OID')
-        quantity = request.values.get('quantity')
-        print(request.values.get('OID'))
-        print(request.values.get('quantity'))
-        # print(quantity)
+        print()
+        # quantity = request.values.get('quantity')
+        # print(request.values.get('OID'))
+        # print(request.values.get('quantity'))
 
-        encrypted_quantity = request.values.get("quantity")
-        decrypted_quantity = decrypt_data(encrypted_quantity)
-        newOid = decrypt_data(oid)
-        # # print(newOid)
-        data = config.cart.find_one({ "_id": ObjectId(newOid.decode())})
-        # # print(decrypted_quantity)
-        # # print(data)
-        filter = data
-        # # print("")  
-        # # # Values to be updated.
-        newvalues = { "$set": { 'quantity': int(decrypted_quantity.decode()) } }
-        config.cart.update_one(filter, newvalues) 
-        return redirect(url_for('product_bp.getAllItems'))
-        # return redirect(url_for('cart_bp.getCartDetails'))
-        # return jsonify(message="Item Updated Successfully", flag=True), 201
+        # encrypted_quantity = request.values.get("quantity")
+        # decrypted_quantity = decrypt_data(encrypted_quantity)
+        # newOid = decrypt_data(oid)
+        # data = config.cart.find_one({ "_id": ObjectId(newOid.decode())})
+
+        # filter = data
+
+        # newvalues = { "$set": { 'quantity': int(decrypted_quantity.decode()) } }
+        # config.cart.update_one(filter, newvalues) 
+        # return redirect(url_for('product_bp.getAllItems'))
+
 
     except (ex.BadRequestKeyError, KeyError):
         return internal_error()
